8259295e24eb9c7b84ed639086f0cb0f02fa41a8
James Ridgway <james@jamesridgway.co.uk>
2022-06-05 14:14:42 +0100

    Refining API

--- app/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import app from './app';
-
-const port = 8080;
-
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`);
-});
--- infra-azure/index.ts
+++ infra-azure/index.ts
@@ -4,14 +4,14 @@ import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 import * as web from "@pulumi/azure-native/web";
 
-// import { getConnectionString, signedBlobReadUrl } from "./helpers";
+import { getConnectionString, signedBlobReadUrl } from "./helpers";
 
 // Create a separate resource group for this example.
 const resourceGroup = new resources.ResourceGroup("gh-approval-api");
 
 // Storage account is required by Function App.
 // Also, we will upload the function code to the same storage account.
-const storageAccount = new storage.StorageAccount("sa-gh-approval", {
+const storageAccount = new storage.StorageAccount("ghapproval", {
     resourceGroupName: resourceGroup.name,
     sku: {
         name: storage.SkuName.Standard_LRS,
@@ -39,25 +39,25 @@ const plan = new web.AppServicePlan("plan", {
     },
 });
 
-// // Build the connection string and zip archive's SAS URL. They will go to Function App's settings.
-// const storageConnectionString = getConnectionString(resourceGroup.name, storageAccount.name);
-// const codeBlobUrl = signedBlobReadUrl(codeBlob, codeContainer, storageAccount, resourceGroup);
+// Build the connection string and zip archive's SAS URL. They will go to Function App's settings.
+const storageConnectionString = getConnectionString(resourceGroup.name, storageAccount.name);
+const codeBlobUrl = signedBlobReadUrl(codeBlob, codeContainer, storageAccount, resourceGroup);
 
-// const app = new web.WebApp("fa", {
-//     resourceGroupName: resourceGroup.name,
-//     serverFarmId: plan.id,
-//     kind: "functionapp",
-//     siteConfig: {
-//         appSettings: [
-//             { name: "AzureWebJobsStorage", value: storageConnectionString },
-//             { name: "FUNCTIONS_EXTENSION_VERSION", value: "~3" },
-//             { name: "FUNCTIONS_WORKER_RUNTIME", value: "node" },
-//             { name: "WEBSITE_NODE_DEFAULT_VERSION", value: "~14" },
-//             { name: "WEBSITE_RUN_FROM_PACKAGE", value: codeBlobUrl },
-//         ],
-//         http20Enabled: true,
-//         nodeVersion: "~14",
-//     },
-// });
+const app = new web.WebApp("fa", {
+    resourceGroupName: resourceGroup.name,
+    serverFarmId: plan.id,
+    kind: "functionapp",
+    siteConfig: {
+        appSettings: [
+            { name: "AzureWebJobsStorage", value: storageConnectionString },
+            { name: "FUNCTIONS_EXTENSION_VERSION", value: "~3" },
+            { name: "FUNCTIONS_WORKER_RUNTIME", value: "node" },
+            { name: "WEBSITE_NODE_DEFAULT_VERSION", value: "~14" },
+            { name: "WEBSITE_RUN_FROM_PACKAGE", value: codeBlobUrl },
+        ],
+        http20Enabled: true,
+        nodeVersion: "~14",
+    },
+});
 
-// export const endpoint = pulumi.interpolate`https://${app.defaultHostName}/api/HelloNode?name=Pulumi`;
+export const endpoint = pulumi.interpolate`https://${app.defaultHostName}/api/HelloNode?name=Pulumi`;
