33b9cba2cc63c67d52a14cfda8d90371478b180d
James Ridgway <james@jamesridgway.co.uk>
2022-06-11 12:00:44 +0100

    Adding body parser to API

--- app/api/app.js
+++ app/api/app.js
@@ -1,27 +1,32 @@
+const bodyParser = require('body-parser');
 const express = require("express");
 const axios = require('axios');
 
+
 const app = express();
+app.use(bodyParser.json())
+
 
 app.get("/api", (req, res) => {
-  const repositoryFullName = req.body.repositoryFullName;
-  const commitHash = req.body.commitHash;
-  const workflowIdToTrigger = req.body.workflowIdToTrigger;
-  const webhookUrl = req.body.webhookUrl;
-
-  const webhookPayload = {
-    text: 'Hello world!'
-  }
-  axios.post(webhookUrl, webhookPayload)
-    .then(response => console.log(`Webhook responded with a ${response.status} response`))
-    .catch(error => console.error('There was an error ', error));
-
   res.json({
     ok: true,
   });
 });
 
 app.post("/api/approval", (req, res) => {
+  console.log(req.body);
+  const repositoryFullName = req.body.repositoryFullName;
+  const commitHash = req.body.commitHash;
+  const workflowIdToTrigger = req.body.workflowIdToTrigger;
+  const webhookUrl = req.body.webhookUrl;
+
+  const webhookPayload = {
+    text: `Triggered by ${repositoryFullName} at commit ${commitHash}. Prompt to kick off workflow ${workflowIdToTrigger}`
+  }
+  axios.post(webhookUrl, webhookPayload)
+    .then(response => console.log(`Webhook responded with a ${response.status} response`))
+    .catch(error => console.error('There was an error ', error));
+
   res.json({
     ok: true,
     desc: 'Approval requested'
